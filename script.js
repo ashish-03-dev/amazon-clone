0d36e5e90a42914080308e44bba7ffb5a4684e55
Ashish Kumar <ashish73e@gmail.com>
2025-01-31 18:25:44 +0530

    added loader transition

--- script.js
+++ script.js
@@ -1,10 +1,37 @@
-window.onload = function () {
-    setTimeout(function () {       
-        let  loaders = document.querySelectorAll(".loader");
-        for(let node of loaders){
-            node.style.display="none";
+// window.onload = function () {
+//     setTimeout(function () {       
+//         let  loaders = document.querySelectorAll(".loader");
+//         for(let node of loaders){
+//             // node.style.display="none";
+//             node.style.opacity="0";
+//             node.style.visibility="hidden";
+//         }
+//         document.querySelector("body").classList.remove("loaderCenter");
+
+//         document.querySelector(".body").style.display="block";
+//         setTimeout(()=>{
+//             document.querySelector(".body").style.opacity="1";
+//         },100);
+//     }, 2500);
+// }; 
+
+document.addEventListener("DOMContentLoaded", function () {
+    setTimeout(function () {
+        let loaders = document.querySelectorAll(".loader");
+        for (let node of loaders) {
+            // node.style.display="none";
+            node.style.opacity = "0";
+            node.style.visibility = "hidden";
         }
-        document.querySelector("body").classList.remove("loaderCenter");
-        document.querySelector(".body").style.display="block";
-    }, 1400);
-}; 
+        setTimeout(() => {
+            for (let node of loaders) {
+                node.style.display = "none";
+            };
+            document.querySelector("body").classList.remove("loaderCenter");
+            document.querySelector(".body").style.display = "block";
+            setTimeout(() => {
+                document.querySelector(".body").style.opacity = "1";
+            },100);
+        }, 300);
+    }, 700);
+});
